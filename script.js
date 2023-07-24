4360a5037bcfc6004181a186a7edecde43f01459
Manoj Saini <manojsaini513@gmail.com>
2023-07-24 12:07:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// complete this js code
 function Person(name, age) {
 	this.name = name;
 	this.age = age;
 }
 Person.prototype.greet = function(){
-	console.log(`Hello, my name is ${this.name}, and I am ${this.age} years old.`)
-};
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+}
 
 function Employee(name, age, jobTitle) {
 	Person.call(this, name, age);
