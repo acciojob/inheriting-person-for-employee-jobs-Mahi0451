f26bd6ce7869aa359456729d46868b05b291bba9
Manoj Saini <manojsaini513@gmail.com>
2023-07-24 12:31:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function Employee(name, age, jobTitle) {
 // Employee.prototype = Object.create(Person.prototype);
 // Employee.prototype.constructor = Employee;
 
-Employee.prototype.__proto__ = person.prototype;
+Employee.prototype.__proto__ = Person.prototype;
 
 
 Employee.prototype.jobGreet = function(){
